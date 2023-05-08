292852a28c559b73dc8dc36f4862497426d61734
salmonfishycooked <salmonfishycooked@gmail.com>
2023-05-08 22:37:51 +0800

    add leetcode 46

---
+++ leetcode/_46/p46.go
@@ -0,0 +1,27 @@
+package _46
+
+func permute(nums []int) (ans [][]int) {
+	n := len(nums)
+	var backtrack func(int)
+	tmp := make([]int, 0, n)
+	vis := make([]bool, n)
+	backtrack = func(level int) {
+		if level == n {
+			t := make([]int, n)
+			copy(t, tmp)
+			ans = append(ans, t)
+			return
+		}
+		for i := 0; i < n; i++ {
+			if !vis[i] {
+				tmp = append(tmp, nums[i])
+				vis[i] = true
+				backtrack(level + 1)
+				vis[i] = false
+				tmp = tmp[:len(tmp)-1]
+			}
+		}
+	}
+	backtrack(0)
+	return
+}
---
+++ leetcode/_46/p46_test.go
@@ -0,0 +1,11 @@
+package _46
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestProblem46(t *testing.T) {
+	nums := []int{1, 2, 3}
+	fmt.Println(permute(nums))
+}
